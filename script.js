e83684520013d302fa2cc59dfad5c171a7f3a697
Padmesh Bhat <Padmeshhu2006@gmail.com>
2026-06-18 23:48:19 +0530

    worked to move elements from left card to right card

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ console.log("1. JS file is connected!");
 const taskInput=document.getElementById("task-input");
 const addButton =document.getElementById("add-button");
 const activeList=document.getElementById("active-list");
+const saveBtn=document.getElementById("save-button");
+const saveList=document.getElementById("saved-list");
 
 console.log("2. The Add Button is:", addButton);
 console.log("3. Button was clicked!");
@@ -19,5 +21,15 @@ addButton.addEventListener("click", function() {
     activeList.appendChild(newListItem);
     taskInput.value="";
    
+
+});
+saveBtn.addEventListener("click",function(){
+    if(activeList.children.length==0){
+        alert("no tasks to save");
+        return;
+    }
+    while(activeList.firstChild){
+        SavedList.appendChild(activeList.firstChild);
+    }
 
 });
