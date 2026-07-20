5f5fa037bffe2e328cf386e01d5e2990904de038
Padmesh Bhat <Padmeshhu2006@gmail.com>
2026-07-20 17:37:19 +0530

    fix(frontend): remove invalid require statement, fix localhost URL, and correct error handling

--- signup.js
+++ signup.js
@@ -1,3 +1,5 @@
+
+
 const Signupform=document.getElementById("signupForm");
 const NewUsername=document.getElementById("newUsername");
 const Newpassword=document.getElementById("newPassword");
@@ -10,9 +12,32 @@ Signupform.addEventListener('submit',function(event) {
     if(npass!==cpass){
         return alert("Password not matches");
     }
-    localStorage.setItem("loggedinuser",NewUsername.value.trim());
-    localStorage.setItem("loggedinpass",npass);
-    alert("Account created...Redirecting...");
-    window.location.href="login.html";
+    const newUser={
+        username:NewUsername.value.trim(),
+        password:npass
+    };
+
+    fetch("http://localhost:3000/api/signup",{
+        method:"POST",
+        headers:{
+            "Content-Type":"application/json"
+        },
+        body:JSON.stringify(newUser)   
+    })
+    .then(response =>response.json())
+    .then(data =>{
+        if(data.error){
+            alert("error:"+data.error);
+        }
+        else{
+            alert("account created successfully! redirecting");
+            window.location.href="login.html";
+        }
+
+    })
+    .catch(error=>{
+        console.error("failed to connect to the server:",error);
+    });
+
 
 });
